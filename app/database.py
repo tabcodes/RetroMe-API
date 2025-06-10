2b9c1566945e23249802ae2e1659e8dbcecd3b89
tabcodes <tabcodes@tabcodes.dev>
2025-06-09 20:39:32 -0400

    Set up application database session

---
+++ app/database.py
@@ -0,0 +1,20 @@
+import os
+from typing import Annotated
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import Depends
+
+DATABASE_URL = os.environ.get("DATABASE_URL", None)
+
+connect_args = {
+    "echo": True
+}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
-from typing import Union
-from fastapi import FastAPI
+from typing import Annotated, Union
+from fastapi import FastAPI, Depends
+from sqlmodel import Field, Session, SQLModel
+from app.database import get_session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+from app.database import create_db_and_tables, get_session
 
 app = FastAPI()
 
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
 @app.get("/")
-def read_root():
+def read_root(session: SessionDep):
     return {"ping": "pong!"}
