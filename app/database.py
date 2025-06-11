731845491bd504391ec31257e43d2c9fa04a77ce
tabcodes <tabcodes@tabcodes.dev>
2025-06-11 19:49:38 -0400

    Extra validation for DB env var

--- app/database.py
+++ app/database.py
@@ -3,7 +3,10 @@ from typing import Annotated
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from fastapi import Depends
 
-DATABASE_URL = os.environ.get("DATABASE_URL", None)
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if(DATABASE_URL == None):
+    raise Exception("No DB credentials found in environment.")
 
 connect_args = {
     "echo": True
