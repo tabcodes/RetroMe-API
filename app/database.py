a4e5dc08c22b36997fb0670aab873da6582ed343
tabcodes <tabcodes@tabcodes.dev>
2025-06-18 21:09:57 -0400

    Remove create_all, unused/unneeded

--- app/database.py
+++ app/database.py
@@ -11,9 +11,6 @@ if(DATABASE_URL == None):
 
 engine = create_engine(DATABASE_URL, echo=True)
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
 def get_session():
     with Session(engine) as session:
         yield session
